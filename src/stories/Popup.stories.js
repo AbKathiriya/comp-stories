08bd5852034d4df131dd3ed772eaad851ab2488d
Abkathiriya <akash.kathiriya@gmail.com>
2020-10-13 18:31:52 +0530

    Added popup component

---
+++ src/stories/Popup.stories.js
@@ -0,0 +1,28 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import Popup from "../components/Popup";
+
+storiesOf("PopUp", module).add("default", () => {
+  class Wrapper extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isOpen: true,
+      };
+    }
+    onClose = () => {
+      this.setState({ isOpen: !this.state.isOpen });
+    };
+
+    render() {
+      const titleNodeHtml = <div style={{ fontSize: "20px", color: "#d84e55" }}>Enter Details</div>;
+      return this.state.isOpen && (
+        <Popup onClose={this.onClose} titleNode={titleNodeHtml}>
+          <div>Add your Custom component here..</div>
+        </Popup>
+      );
+    }
+  }
+  return <Wrapper />;
+});
